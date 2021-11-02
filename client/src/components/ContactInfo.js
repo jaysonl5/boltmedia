7b4c3a519704b5d9c1cc5a8ce813b82b79e58cd0
Jayson Lewis <jayson.lewis5@gmail.com>
2021-11-02 00:09:19 -0500

    Contact form and info changes

--- client/src/components/ContactInfo.js
+++ client/src/components/ContactInfo.js
@@ -3,9 +3,23 @@
 export default function ContactInfo(props){
 
     return(
-        <div>
+        <div className="ContactInfo">
+            <div className="contactGroup">
+                <p>
+                <i className={'fas fa-phone'} style={{color: "white"}}></i> 918.527.0315</p>
+                <p>
+                <i className={'fas fa-envelope'} style={{color: "white"}}></i> contact@boltmediaokc.com
+                </p>
+                
+            </div>
+
+            <div className="contactGroup">
             <i className={'fab fa-github fa-2x'} style={{color: "white"}} ></i>
             <i className={'fab fa-twitter fa-2x'} style={{color: "white"}} ></i>
+            <i className={'fab fa-linkedin fa-2x'} style={{color: "white"}} ></i>
+            <i className={'fab fa-instagram fa-2x'} style={{color: "white"}} ></i>
+            </div>
+            
         </div>
     )
     
