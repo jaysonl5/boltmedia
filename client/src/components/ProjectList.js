de72253a270170f80e8d4b19dea3d8b145dbb0ad
Jayson Lewis <jayson.lewis5@gmail.com>
2021-11-01 22:03:06 -0500

    Further styled cards and icon badge

--- client/src/components/ProjectList.js
+++ client/src/components/ProjectList.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faExternalLinkAlt } from '@fortawesome/free-solid-svg-icons'
+import { faChevronRight } from '@fortawesome/free-solid-svg-icons'
 import TechIcons from './TechIcons';
 
 function ProjectList(props) {
@@ -28,16 +28,18 @@ function ProjectList(props) {
          :
          data.projects.map(item => (
           <div className="projectList projectList-animate">
+            <div className="techIcons">
+                <TechIcons techs={item.tech} /> 
+              </div>
             <img src={`./images/${item.img}`} alt={`Placeholder for ${item.title} project`} /> 
             <div className="projectCards">                                                   
               <h4>{item.title}</h4>
-              <div className="icons">
-                <TechIcons techs={item.tech} /> 
-              </div>
-              <p>{item.description} <a href="" onClick={() => window.open(item.link, "_blank")}>Take a closer look <span className="" 
-              role="img">&#128064;</span> </a></p>
-              {/* <button className="visitBtn" onClick={() => window.open(item.link, "_blank")}><FontAwesomeIcon icon={faExternalLinkAlt} /> View</button> */}
+              <p>{item.description} </p>
+               {/* <a href="" onClick={() => window.open(item.link, "_blank")}>Take a closer look <span className="" 
+              role="img">&#128064;</span> </a> */}
+              
             </div>
+            <button className="visitBtn" onClick={() => window.open(item.link, "_blank")}><FontAwesomeIcon icon={faChevronRight} /> View</button>
           </div>
         ))
          
--- client/src/components/TechIcons.js
+++ client/src/components/TechIcons.js
@@ -14,7 +14,7 @@ export default function TechIcons(props){
             return (
             <div style={{display: "inline", margin: "0px 0px 0px 0px", padding: "5px"}}>
                 {/* <FontAwesomeIcon icon={["fas", item.icon]} size="2x" style={{color: item.color}} /> */}
-                <i className={'fab ' + item.icon + ' '} style={{color: "#23376E"}} ></i>
+                <i className={'fab ' + item.icon + ' '} style={{color: "white"}} ></i>
 
             </div>
             )
