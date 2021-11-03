a45b1d0eb8463219be21e392a8e09b8e283830eb
Jayson <jayson.lewis5@gmail.com>
2021-11-03 14:26:20 -0500

    contact form styling adjustments

--- client/src/components/ContactInfo.js
+++ client/src/components/ContactInfo.js
@@ -14,10 +14,10 @@ export default function ContactInfo(props){
             </div>
 
             <div className="contactGroup">
-                <i className={'fab fa-github fa-2x'} style={{color: "white"}} ></i>
-                <i className={'fab fa-twitter fa-2x'} style={{color: "white"}} ></i>
-                <i className={'fab fa-linkedin fa-2x'} style={{color: "white"}} ></i>
-                <i className={'fab fa-instagram fa-2x'} style={{color: "white"}} ></i>
+                <a className="socialIcon" href="https://github.com/jaysonl5" target="_blank"><i className={'fab fa-github fa-2x'} ></i></a>
+                <a className="socialIcon" href="https://twitter.com/boltokc" target="_blank"><i className={'fab fa-twitter fa-2x'} ></i></a>
+                <a className="socialIcon" href="https://www.linkedin.com/in/jayson-lewis-90737951/" target="_blank"><i className={'fab fa-linkedin fa-2x'} ></i></a>
+                <a className="socialIcon" href="https://www.instagram.com/boltmediaokc/" target="_blank"><i className={'fab fa-instagram fa-2x'} ></i></a>
             </div>
             
         </div>
