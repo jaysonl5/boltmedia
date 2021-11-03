c7c03b58bf4ff1d221a03a35971d52850299a233
Jayson <jayson.lewis5@gmail.com>
2021-11-03 14:16:36 -0500

    Styling changes for dynamic contact form

--- client/src/components/ContactInfo.js
+++ client/src/components/ContactInfo.js
@@ -6,18 +6,18 @@ export default function ContactInfo(props){
         <div className="ContactInfo">
             <div className="contactGroup">
                 <p>
-                <i className={'fas fa-phone'} style={{color: "white"}}></i> 918.527.0315</p>
+                    <i className={'fas fa-phone'} style={{color: "white"}}></i> 918.527.0315</p>
                 <p>
-                <i className={'fas fa-envelope'} style={{color: "white"}}></i> contact@boltmediaokc.com
+                    <i className={'fas fa-envelope'} style={{color: "white"}}></i> contact@boltmediaokc.com
                 </p>
                 
             </div>
 
             <div className="contactGroup">
-            <i className={'fab fa-github fa-2x'} style={{color: "white"}} ></i>
-            <i className={'fab fa-twitter fa-2x'} style={{color: "white"}} ></i>
-            <i className={'fab fa-linkedin fa-2x'} style={{color: "white"}} ></i>
-            <i className={'fab fa-instagram fa-2x'} style={{color: "white"}} ></i>
+                <i className={'fab fa-github fa-2x'} style={{color: "white"}} ></i>
+                <i className={'fab fa-twitter fa-2x'} style={{color: "white"}} ></i>
+                <i className={'fab fa-linkedin fa-2x'} style={{color: "white"}} ></i>
+                <i className={'fab fa-instagram fa-2x'} style={{color: "white"}} ></i>
             </div>
             
         </div>
