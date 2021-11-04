e3238ce0fc1852b1991fbc67fae0fda9145b3a61
Jayson <jayson.lewis5@gmail.com>
2021-11-03 19:37:58 -0500

    slight adjustments for heroku build

--- client/src/components/TechIcons.js
+++ client/src/components/TechIcons.js
@@ -1,21 +1,10 @@
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { faReact, faNode, faBootstrap, faCss3 } from '@fortawesome/free-brands-svg-icons'
-import { faDatabase } from '@fortawesome/free-solid-svg-icons'
-
 export default function TechIcons(props){
-
-
-    
-
     return(
-    <div>
-        
+    <div>        
         {props.techs.map(item => {
             return (
-            <div style={{display: "inline", margin: "0px 0px 0px 0px", padding: "5px"}}>
-                {/* <FontAwesomeIcon icon={["fas", item.icon]} size="2x" style={{color: item.color}} /> */}
-                <i className={'fab ' + item.icon + ' '} style={{color: "white"}} ></i>
-
+            <div style={{display: "inline", padding: "5px"}}>             
+                <i className={item.icon + ' '} style={{color: "white"}} ></i>
             </div>
             )
         }
