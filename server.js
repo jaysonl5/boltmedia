f4b2264d16d7c190659545a2280d0a5ab1973976
Jayson Lewis <jayson.lewis5@gmail.com>
2022-11-06 20:58:04 -0600

    feature/test: Added a console log

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const bodyParser = require('body-parser');
 const app = express();
 const port = process.env.PORT || 5000;
 
+console.log("This is a test for commiting changes");
 
 app.use(cors());
 app.use(express.json());
